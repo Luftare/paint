ec22d48b32553281f0ebc95a03e4781adeabe993
luftare <ilmari.koskinen1@gmail.com>
2019-03-08 13:53:29 +0200

    Add fullscreen

--- Paint.js
+++ Paint.js
@@ -11,6 +11,13 @@ class Paint {
     };
   }
 
+  fullScreen() {
+    window.addEventListener('resize', () => {
+      this.canvas.width = window.innerWidth;
+      this.canvas.height = window.innerHeight;
+    })
+  }
+
   setViewAngle(angle) {
     this.viewTransform.angle = angle;
   }
